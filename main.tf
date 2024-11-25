1733f479406e5730a4f1cb3d4fab883e219e02c3
bidaoui4905 <abderrahman.hbilate@gmail.com>
2024-11-25 23:46:22 +0100

    initial main.tf variables.tf

--- main.tf
+++ main.tf
@@ -1,44 +1,150 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "5.13.0"
-    }
+provider "aws" {
+  region = var.aws_region
+}
+
+# VPC Resources
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "${var.project_name}-vpc"
   }
 }
 
-provider "google" {
-  credentials = "keys/my-credentials.json"
-  project     = var.project_id
-  region      = "europe-southwest1"
-}
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_cidr
+  map_public_ip_on_launch = true
 
-resource "google_storage_bucket" "data-lake-bucket" {
-  name          = "${var.project_name}-terra-bucket"
-  location      = "EU"
-  force_destroy = true
-
-  # Optional, but recommended settings:
-  storage_class               = "STANDARD"
-  uniform_bucket_level_access = true
-
-  versioning {
-    enabled = true
-  }
-
-  lifecycle_rule {
-    action {
-      type = "Delete"
-    }
-    condition {
-      age = 30 // days
-    }
+  tags = {
+    Name = "${var.project_name}-public-subnet"
   }
 }
 
-resource "google_bigquery_dataset" "dataset" {
-  dataset_id = "demo_dataset"
-  project    = var.project_id
-  location   = "EU"
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
 }
 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+# Security Group
+resource "aws_security_group" "instances" {
+  name        = "${var.project_name}-sg"
+  description = "Security group for cyberrange instances"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.allowed_ip]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = [var.allowed_ip]
+    description = "Juice Shop access"
+  }
+
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    self      = true
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instances
+resource "aws_instance" "kali" {
+  ami           = var.kali_ami
+  instance_type = var.instance_types["kali"]
+  key_name      = var.key_name
+
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.instances.id]
+  associate_public_ip_address = true
+
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp2"
+    encrypted   = true
+  }
+
+  tags = {
+    Name = "${var.project_name}-kali"
+    Role = "red-team"
+  }
+}
+
+resource "aws_instance" "blue_team" {
+  ami           = var.ubuntu_ami
+  instance_type = var.instance_types["blue"]
+  key_name      = var.key_name
+
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.instances.id]
+  associate_public_ip_address = true
+
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp2"
+    encrypted   = true
+  }
+
+  tags = {
+    Name = "${var.project_name}-blue"
+    Role = "blue-team"
+  }
+}
+
+resource "aws_instance" "target" {
+  ami           = var.ubuntu_ami
+  instance_type = var.instance_types["target"]
+  key_name      = var.key_name
+
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.instances.id]
+  associate_public_ip_address = true
+
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp2"
+    encrypted   = true
+  }
+
+  tags = {
+    Name = "${var.project_name}-target"
+    Role = "target"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -1,9 +1,58 @@
-variable "project_name" {
-  description = "The name of the project"
-  type = string
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-west-2"
 }
 
-variable "project_id" {
-  description = "The id of the project"
-  type = string
+variable "project_name" {
+  description = "Project name to be used for tagging"
+  type        = string
+  default     = "cyberrange"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  description = "CIDR block for subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "allowed_ip" {
+  description = "IP address allowed to connect to instances"
+  type        = string
+  default     = "0.0.0.0/0"  # Replace with our IP
+}
+
+variable "key_name" {
+  description = "Name of SSH key pair"
+  type        = string
+  default     = "cyberrange-key"
+}
+
+# Instance specific variables
+variable "kali_ami" {
+  description = "Kali Linux AMI ID"
+  type        = string
+  default     = "ami-0b7061ad80874a692"  # Replace with latest Kali Linux AMI (we can make it dynamicly later)
+}
+
+variable "ubuntu_ami" {
+  description = "Ubuntu LTS AMI ID"
+  type        = string
+  default     = "ami-0c65adc9a5c1b5d7c"  # Replace with latest Ubuntu 22.04 LTS AMI (we can make it dynamicly later)
+}
+
+variable "instance_types" {
+  description = "Instance types for different purposes"
+  type        = map(string)
+  default = {
+    kali    = "t2.micro"
+    blue    = "t2.micro"
+    target  = "t2.micro"
+  }
 }
